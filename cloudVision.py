7cee2ff06607541c60edb8ed03b97dd880af7d4b
Miguel Pessoa <guelmisp@gmail.com>
2018-01-02 19:16:55 -0200

    Insert face dection using google cloud

--- cloudVision.py
+++ cloudVision.py
@@ -11,7 +11,7 @@ client = vision.ImageAnnotatorClient()
 # The name of the image file to annotate
 file_name = os.path.join(
     os.path.dirname(__file__),
-    'resourcers/cat.jpg')
+    'resourcers/test.jpg')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
@@ -25,4 +25,22 @@ labels = response.label_annotations
 
 print('Labels:')
 for label in labels:
-    print(label.description)
+  print(label.description)
+
+# Performs label detection on the image file
+response = client.face_detection(image=image)
+faces = response.face_annotations
+
+# Names of likelihood from google.cloud.vision.enums
+likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+print('Faces:')
+
+for face in faces:
+  print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+  print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+  print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+
+  vertices = (['({},{})'.format(vertex.x, vertex.y) for vertex in face.bounding_poly.vertices])
+
+  print('face bounds: {}'.format(','.join(vertices)))
